f0ad086c40ad845b7f8b3421e7a92a8dfdbed616
Saurabh Chauhan <saurabh311@gmail.com>
2020-11-09 13:55:19 +0100

    Added Home button

--- src/components/molecules/Footer.jsx
+++ src/components/molecules/Footer.jsx
@@ -1,6 +1,5 @@
 //React Core
 import React from "react";
-import { Link } from "react-router-dom";
 import "../../style/footer.css"
 
 export default function Footer() {
--- src/components/templates/Customer.jsx
+++ src/components/templates/Customer.jsx
@@ -1,15 +1,42 @@
-/*import React from "react";
-import Result from "../molecules/Result";
 
-export default function CustomerPage ({match, information}) {
-    const { user_phone,User_name } = data;
-    const query =new RegExp(match.params.query, "i");
-    const result = information.filter((item) => item.user_phone.match(query));
+import React from "react";
 
-    return (
-        <div className = "Customer-Page">
-        <h1> Welcome Page</h1>
-        <AllParcel data={result}/>
-        </div>
-    );
-}*/
+//Other imports
+
+import Search from "../molecules/Search";
+import AllParcel from "../molecules/AllParcel";
+
+
+export default function CustomerPage({ parameter, information }) {
+
+    const result = information.filter((item) => { return (item.user_name) === (parameter) });
+    if (result[0] === undefined) {
+        return (
+            <div>
+                <h1>No such user. Please Check the Username entered!!</h1>
+            </div>
+        );
+    } else
+        return (
+            <div className="Customer-Page">
+                <h2 className="welcome-tag">Welcome {result[0].user_name}</h2>
+                <Search />
+                <table className="tab">
+                    <thead>
+                        <tr className="rows">
+                            <th>Parcel_ID</th>
+                            <th>Sender</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                </table>
+                {information.map(item => (
+                    <AllParcel
+                        key={item.id}
+                        parcel_id={item.parcel_id}
+                        sender={item.sender}
+                        status={item.status} />
+                ))}
+            </div>
+        );
+}
